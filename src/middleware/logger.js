5d9ef748ba62c98d46539754a9312590b84ba8ae
Paul Haddad <paulh16@gmail.com>
2018-01-30 07:55:18 -0500

    Refactor middleware to use ES6 arrow functions

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,10 +1,6 @@
-export default function(store) {
-  return function(next) {
-    return function(action) {
-      const result = next(action);
-      console.log('Action:', action);
-      console.log('State:', store.getState());
-      return result;
-    }
-  }
+export default store => next => action => {
+  const result = next(action);
+  console.log('Action:', action);
+  console.log('State:', store.getState());
+  return result;
 }
